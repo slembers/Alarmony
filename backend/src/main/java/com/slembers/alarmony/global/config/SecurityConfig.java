ba084400fa9d876365d4bc9bd683fc466d8dff11
홍금비 <canbe159@gmail.com>
2023-04-19 16:55:59 +0900

    fix : SecurityConfig 수정

--- backend/src/main/java/com/slembers/alarmony/global/config/SecurityConfig.java
+++ backend/src/main/java/com/slembers/alarmony/global/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.slembers.alarmony.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,14 +16,34 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
+        return http
+                // rest api만 사용
                 .httpBasic().disable()
+                .cors().and()
                 .csrf().disable()
+                // iframe 요소를 통한 전송 허용
+                .headers()
+                .frameOptions().sameOrigin()
+                .and()
+                //
                 .authorizeRequests()
-                .antMatchers("/error/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/**");
-        return http.build();
+//                .antMatchers("/h2-console/**").permitAll() // 추가
+              /*  .antMatchers("/members/signup", "/members/login").permitAll()
+                .antMatchers(HttpMethod.PUT,"/boards/gif/{gifId}").authenticated()
+                .antMatchers(HttpMethod.DELETE,"/boards/gif/{boardId}").authenticated()
+                .antMatchers(HttpMethod.DELETE,"/boards/{boardId}").authenticated()
+                .antMatchers(HttpMethod.POST,"/boards/{boardId}").authenticated()
+                .antMatchers(HttpMethod.PUT,"/boards/{boardId}").authenticated()
+                .antMatchers(HttpMethod.POST,"/temp").authenticated()
+                .antMatchers(HttpMethod.GET,"/temp/all").hasAnyAuthority("ROLE_MANAGER")
+                .antMatchers(HttpMethod.PUT,"/temp/{tempId}").hasAnyAuthority("ROLE_MANAGER")
+                .antMatchers(HttpMethod.DELETE,"/temp/{tempId}").hasAnyAuthority("ROLE_MANAGER")
+                .antMatchers(HttpMethod.POST, "/comments/**").authenticated()
+                .antMatchers(HttpMethod.DELETE,"/comments/**").authenticated()*/
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and().build();
     }
+
+
 }
