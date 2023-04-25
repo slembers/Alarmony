00bc5b69a6e930fa079144905171049153358a19
canbe159 <canbe159@gmail.com>
2023-04-26 00:47:34 +0900

    feat: 인가 JwtAuthorizationFilter 추가

--- backend/src/main/java/com/slembers/alarmony/global/config/SecurityConfig.java
+++ backend/src/main/java/com/slembers/alarmony/global/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.slembers.alarmony.global.config;
 
 import com.slembers.alarmony.global.jwt.CustomAuthenticationProvider;
+import com.slembers.alarmony.global.jwt.JwtAuthorizationFilter;
 import com.slembers.alarmony.global.jwt.JwtTokenProvider;
 import com.slembers.alarmony.global.jwt.auth.PrincipalDetailsService;
 import com.slembers.alarmony.global.jwt.filter.CustomAuthenticationFilter;
 import com.slembers.alarmony.global.jwt.handler.CustomLoginSuccessHandler;
+import com.slembers.alarmony.global.redis.repository.RefreshTokenRepository;
+import com.slembers.alarmony.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
@@ -23,6 +27,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -41,6 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter   {
     private final PrincipalDetailsService principalDetailsService;
     private final JwtTokenProvider jwtTokenProvider;
 
+    private  final RefreshTokenRepository refreshTokenRepository;
+    private final MemberRepository memberRepository;
+
 
     /**
      * PasswordEncoder를 Bean으로 등록
@@ -51,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter   {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+      /*  http.csrf().disable().authorizeRequests()
                 // 토큰을 활용하는 경우 모든 요청에 대해 접근이 가능하도록 함
                 .antMatchers("/members/login").permitAll()
                 .anyRequest().permitAll()
@@ -62,7 +70,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter   {
                 // form 기반의 로그인에 대해 비활성화 한다.
                 .formLogin()
                 .disable()
-                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthorizationFilter(jwtTokenProvider),  UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler());*/
+
+
+
+        http
+
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable().
+
+        addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthorizationFilter(jwtTokenProvider),  UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+
     }
 
 
@@ -82,7 +115,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter   {
     @Bean
     public CustomLoginSuccessHandler customLoginSuccessHandler() {
         log.info("여긴머임");
-        return new CustomLoginSuccessHandler(jwtTokenProvider);
+        return new CustomLoginSuccessHandler(jwtTokenProvider,refreshTokenRepository);
     }
 
     @Bean
@@ -93,5 +126,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter   {
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
         authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider());
+    }
+
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler(){
+        return (request, response, ex) -> {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.getWriter().write("가입이 완료되지 않았습니다. 이메일함에 인증 메일을 통해 가입을 완료해주세요");
+        };
     }
 }
