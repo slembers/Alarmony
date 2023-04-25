ad71ce0f838e94c9a484debde3d2efeaefd4331f
박성완 <swany0509@naver.com>
2023-04-25 13:23:18 +0900

    feature : fcm 알림 설정을 위한 기본 세팅

--- android/app/src/main/java/com/slembers/alarmony/MainActivity.kt
+++ android/app/src/main/java/com/slembers/alarmony/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.slembers.alarmony
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        printRegistrationToken()
+    }
+
+    /**
+     * 기기에 등록된 토큰을 출력한다.
+     */
+    @SuppressLint("StringFormatInvalid")
+    private fun printRegistrationToken() {
+        //토큰 출력
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val token = task.result
+                println("FCM 등록 토큰: $token")
+            } else {
+                println("등록 토큰 가져오기 실패")
+            }
+        }
     }
 }
