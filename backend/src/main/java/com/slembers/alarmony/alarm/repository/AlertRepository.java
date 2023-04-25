eb0abfe7dd8913522ce5a77f2f09a54c5f6e0970
박성완 <swany0509@naver.com>
2023-04-25 14:41:47 +0900

    refactor : Notification -> Alert 테이블 및 파일 이름 변경, FCM 메소드 추가

--- backend/src/main/java/com/slembers/alarmony/alarm/repository/AlertRepository.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/repository/AlertRepository.java
@@ -1,9 +1,9 @@
 package com.slembers.alarmony.alarm.repository;
 
-import com.slembers.alarmony.alarm.entity.Notification;
+import com.slembers.alarmony.alarm.entity.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface AlertRepository extends JpaRepository<Alert, Long> {
 }
