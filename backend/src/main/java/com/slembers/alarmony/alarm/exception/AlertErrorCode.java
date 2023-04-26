3a734e4f0c6ae21b9aeb41c658935c4db7b19f44
박성완 <swany0509@naver.com>
2023-04-26 15:31:03 +0900

    feat : 알람, 알림, 멤버알림 에러 코드 추가

--- backend/src/main/java/com/slembers/alarmony/alarm/exception/AlertErrorCode.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/exception/AlertErrorCode.java
@@ -11,8 +11,9 @@ public enum AlertErrorCode implements ErrorCode {
 
     ALERT_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알림 삭제 중 오류가 발생했습니다."),
 
-    ALERT_NOT_FOUND(HttpStatus.NOT_FOUND,"알림이 존재하지 않거나 조회 중 에러가 발생했습니다.")
-    ;
+    ALERT_INVITE_SEND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "초대 알림 설정 중 에러가 발생했습니다."),
+
+    ALERT_NOT_FOUND(HttpStatus.NOT_FOUND, "알림이 존재하지 않거나 조회 중 에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
