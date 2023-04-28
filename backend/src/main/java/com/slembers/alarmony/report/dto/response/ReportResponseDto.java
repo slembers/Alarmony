87e481ede07153de815b4362d0ed6b0ea5aea658
안수빈 <sbsc20@naver.com>
2023-04-28 13:40:18 +0900

    fix: null이면 출력하지 않도록 수정

--- backend/src/main/java/com/slembers/alarmony/report/dto/response/ReportResponseDto.java
+++ backend/src/main/java/com/slembers/alarmony/report/dto/response/ReportResponseDto.java
@@ -1,5 +1,6 @@
 package com.slembers.alarmony.report.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReportResponseDto {
 
     private Long reportId;
