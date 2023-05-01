90972a2473e9024c670486f78d936b8ce9f86301
홍금비 <canbe159@gmail.com>
2023-05-01 18:03:31 +0900

    feat: 토큰 재발급 ReissueTokenDto 추가

--- backend/src/main/java/com/slembers/alarmony/member/dto/request/ReissueTokenDto.java
+++ backend/src/main/java/com/slembers/alarmony/member/dto/request/ReissueTokenDto.java
@@ -1,18 +1,24 @@
 package com.slembers.alarmony.member.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 
+@Builder
 @ToString
 @Getter
+@Setter
 public class ReissueTokenDto {
 
-
+    @JsonProperty(value = "grant_type")
     private String grantType;
-
+    @JsonProperty(value = "username")
     private String username;
 
+    @JsonProperty(value = "refresh_token")
     private String refreshToken;
 
 
