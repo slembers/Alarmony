3afa0cc67c65c6d0b536f9a51a9376ce357091fa
안수빈 <sbsc20@naver.com>
2023-05-01 13:24:41 +0900

    fix: #9 알람 현황 api response 수정

--- backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmRecordDto.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmRecordDto.java
@@ -1,6 +1,5 @@
 package com.slembers.alarmony.alarm.dto;
 
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +11,6 @@ public class AlarmRecordDto {
 
     String nickname;
     String profileImg;
-    LocalDateTime todayAlarmRecord;
+    boolean success;
 
 }
