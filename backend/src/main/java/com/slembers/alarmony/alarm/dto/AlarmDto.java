1317f7807b000a3b763b69f46d143128e58a080e
박성완 <swany0509@naver.com>
2023-05-02 14:50:31 +0900

    refactor : Alarm 공용 DTO 수정

--- backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmDto.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmDto.java
@@ -1,11 +1,12 @@
 package com.slembers.alarmony.alarm.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
+import com.slembers.alarmony.global.util.CommonMethods;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Builder
+@NoArgsConstructor
 public class AlarmDto {
 
     @JsonProperty(value = "alarm_id")
@@ -20,9 +21,22 @@ public class AlarmDto {
     @JsonProperty(value = "minute")
     private int minute;
 
-    @JsonProperty(value = "ampm")
-    private String ampm;
-
     @JsonProperty(value = "alarm_date")
-    private List<String> alarmDate;
+    private List<Boolean> alarmDate;
+
+    /**
+     *
+     * @param alarmId 알람 아이디
+     * @param title 제목
+     * @param hour 시간
+     * @param minute 분
+     * @param alarmDate 알람 요일 정보
+     */
+    public AlarmDto(Long alarmId, String title, int hour, int minute, String alarmDate) {
+        this.alarmId = alarmId;
+        this.title = title;
+        this.hour = hour;
+        this.minute = minute;
+        this.alarmDate = CommonMethods.changeStringToBooleanList(alarmDate);
+    }
 }
