28276576885c16583e366428550ba276c411eb5c
박성완 <swany0509@naver.com>
2023-05-03 11:09:50 +0900

    refactor : Alert 검색 방식 변경

--- backend/src/main/java/com/slembers/alarmony/alarm/repository/AlertRepository.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/repository/AlertRepository.java
@@ -1,8 +1,10 @@
 package com.slembers.alarmony.alarm.repository;
 
+import com.slembers.alarmony.alarm.dto.AlertDto;
 import com.slembers.alarmony.alarm.entity.Alert;
 import com.slembers.alarmony.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +17,9 @@ public interface AlertRepository extends JpaRepository<Alert, Long> {
      * @param receiver 받은 사람
      * @return 알림 목록
      */
-    List<Alert> findAllByReceiver(Member receiver);
+    @Query("select new com.slembers.alarmony.alarm.dto.AlertDto(a.id, m.profileImgUrl, a.content, a.type) " +
+            "from alert a inner join member m " +
+            "on a.sender.id = m.id " +
+            "where a.receiver = :receiver ")
+    List<AlertDto> findMemberAlertDtos(Member receiver);
 }
