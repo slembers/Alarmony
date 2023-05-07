920a181d44663d195c9498388470042a206dee63
tayo5712 <kimty5712@gmail.com>
2023-05-08 01:12:17 +0900

    Feat : 알람 세팅

--- android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmManager.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmManager.kt
@@ -1,7 +1,13 @@
 package com.slembers.alarmony.feature.alarm
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.icu.util.Calendar
+import android.os.Build
 
 fun saveAlarm(mAlarmViewModel: AlarmViewModel, alarm: Alarm, context: Context) {
     mAlarmViewModel.addAlarm(alarm)
@@ -22,4 +28,40 @@ fun setAlarm(context: Context, alarm: Alarm) {
     if (curTime > newTime) {    // 설정한 시간이, 현재 시간 보다 작다면 바로 울리기 때문에 다음날로 설정
         newTime += intervalDay
     }
+
+    val intent = Intent(context, AlarmReceiver::class.java)
+    intent.putExtra("alarm", alarm)
+    val alarmIntentRTC: PendingIntent =
+        PendingIntent.getBroadcast(
+            context,
+            alarm.alarm_id.toInt(),
+            intent,
+            PendingIntent.FLAG_MUTABLE
+        )
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                newTime,
+                alarmIntentRTC
+            )
+
+        }
+        else -> {
+            alarmManager.setExact(
+                AlarmManager.RTC_WAKEUP,
+                newTime,
+                alarmIntentRTC
+            )
+        }
+    }
+
+    val receiver = ComponentName(context, AlarmReceiver::class.java)
+    context.packageManager.setComponentEnabledSetting(
+        receiver,
+        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+        PackageManager.DONT_KILL_APP
+    )
 }
