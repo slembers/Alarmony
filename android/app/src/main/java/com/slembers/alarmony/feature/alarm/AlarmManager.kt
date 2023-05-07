2fd8d835b00d3ab23f70b50c81ac5249a67ebc1b
tayo5712 <kimty5712@gmail.com>
2023-05-08 00:42:35 +0900

    Feat : 알람 초기 설정

--- android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmManager.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmManager.kt
@@ -1,7 +1,25 @@
 package com.slembers.alarmony.feature.alarm
 
 import android.content.Context
+import android.icu.util.Calendar
 
-fun saveAlarm(mAlarmViewModel: AlarmViewModel, alarm: Alarm) {
+fun saveAlarm(mAlarmViewModel: AlarmViewModel, alarm: Alarm, context: Context) {
     mAlarmViewModel.addAlarm(alarm)
+    setAlarm(context, alarm)
+}
+
+fun setAlarm(context: Context, alarm: Alarm) {
+    val calendar: Calendar = Calendar.getInstance()
+    val intervalDay : Long = 24*60*60*1000 // 24시간
+    calendar.set(Calendar.HOUR_OF_DAY, alarm.hour)
+    calendar.set(Calendar.MINUTE, alarm.minute)
+    calendar.set(Calendar.SECOND, 0)
+    calendar.set(Calendar.MILLISECOND, 0)
+
+    var newTime = calendar.timeInMillis
+    var curTime = System.currentTimeMillis()
+
+    if (curTime > newTime) {    // 설정한 시간이, 현재 시간 보다 작다면 바로 울리기 때문에 다음날로 설정
+        newTime += intervalDay
+    }
 }
