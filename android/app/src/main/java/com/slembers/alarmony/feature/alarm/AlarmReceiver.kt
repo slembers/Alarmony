ef8973a20e76636523835e7a099f68030587cfaa
tayo5712 <kimty5712@gmail.com>
2023-05-08 01:20:25 +0900

    Feat : AlarmReceiver 작성

--- android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmReceiver.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmReceiver.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.getIntent
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.slembers.alarmony.util.Constants.BOOT_COMPLETED
 import com.slembers.alarmony.util.Constants.OPEN_TYPE
@@ -11,8 +14,28 @@ import com.slembers.alarmony.util.Constants.REFRESH
 import com.slembers.alarmony.util.Constants.alarm_id
 
 class AlarmReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
+        val newIntent = Intent(context, AlarmsForegroundService::class.java)
 
+        if (intent.action == BOOT_COMPLETED) {
+            newIntent.putExtra(OPEN_TYPE, REFRESH)
+        } else {
+            val alarm = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                intent.getParcelableExtra("alarm", Alarm::class.java)
+            } else {
+                intent.getParcelableExtra<Alarm>("alarm")
+            }
+            val calendar: Calendar = Calendar.getInstance()
+            var todayDayOfWeek: Int = calendar.get(Calendar.DAY_OF_WEEK) - 2    // 오늘 요일 구하기
+            if (todayDayOfWeek == -1) todayDayOfWeek = 6
+            if (alarm!!.alarm_date[todayDayOfWeek] == false) return // 오늘이 울리는 요일이 아니면 리턴
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                context.startForegroundService(newIntent)
+            } else {
+                context.startService(newIntent)
+            }
+        }
     }
 
 }
