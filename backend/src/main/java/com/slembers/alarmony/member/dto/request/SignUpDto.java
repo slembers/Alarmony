8fd93ead4b1703d44a622f5e3dbf97e9dd4e4f9a
홍금비 <canbe159@gmail.com>
2023-05-08 10:36:42 +0900

    refactor: 비밀번호 영문(대/소문자) + 숫자 조합으로 가입할 수 있도록 정규식 변경

--- backend/src/main/java/com/slembers/alarmony/member/dto/request/SignUpDto.java
+++ backend/src/main/java/com/slembers/alarmony/member/dto/request/SignUpDto.java
@@ -21,7 +21,7 @@ public class SignUpDto {
     @JsonProperty(value = "username")
     private String username;
 
-    @Pattern(regexp ="^[a-z\\d]{8,16}$", message = "비밀번호는 영문 숫자를 포함하여 8자리 이상 16자리 이하여야 합니다.")
+    @Pattern(regexp ="^[a-zA-Z\\d]{8,16}$", message = "비밀번호는 영문(대/소문자) 숫자를 포함하여 8자리 이상 16자리 이하여야 합니다.")
     @JsonProperty(value = "password")
     private String password;
 
