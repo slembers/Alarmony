e99b74d028dd8a3288041c040aad25043265c5df
tayo5712 <kimty5712@gmail.com>
2023-05-09 00:31:51 +0900

    알람 삭제 및 알ë 취소

--- android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmManager.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/alarm/AlarmManager.kt
@@ -4,10 +4,12 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
+import android.content.Context.ALARM_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.icu.util.Calendar
 import android.os.Build
+
 
 fun saveAlarm(mAlarmViewModel: AlarmViewModel, alarm: Alarm, context: Context) {
     mAlarmViewModel.addAlarm(alarm)
@@ -39,7 +41,7 @@ fun setAlarm(context: Context, alarm: Alarm) {
             intent,
             PendingIntent.FLAG_MUTABLE
         )
-    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
     when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
             alarmManager.setExactAndAllowWhileIdle(
@@ -64,4 +66,16 @@ fun setAlarm(context: Context, alarm: Alarm) {
         PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
         PackageManager.DONT_KILL_APP
     )
+}
+
+fun deleteAlarm(mAlarmViewModel: AlarmViewModel, alarm: Alarm, context: Context) {
+    mAlarmViewModel.deleteAlarm(alarm)
+    cancelAlarm(context, alarm)
+}
+
+fun cancelAlarm(context : Context, alarm : Alarm) {
+    val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
+    val intent = Intent(context, AlarmReceiver::class.java)
+    val pendingIntent = PendingIntent.getBroadcast(context, alarm.alarm_id.toInt(), intent, PendingIntent.FLAG_MUTABLE)
+    alarmManager.cancel(pendingIntent)
 }
