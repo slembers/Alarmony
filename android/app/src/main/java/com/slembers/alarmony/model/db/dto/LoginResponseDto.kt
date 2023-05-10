a373fa24e74c3d62d1d5f9d09b5eacfd274409bb
박성완 <swany0509@naver.com>
2023-05-10 16:49:07 +0900

    refactor : 변수 이름 camel 방식으로 변경

--- android/app/src/main/java/com/slembers/alarmony/model/db/dto/LoginResponseDto.kt
+++ android/app/src/main/java/com/slembers/alarmony/model/db/dto/LoginResponseDto.kt
@@ -3,9 +3,9 @@ package com.slembers.alarmony.model.db.dto
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponseDto (
-    @SerializedName("token_type") val tokenType : String?,
-    @SerializedName("access_token") val accessToken : String?,
-    @SerializedName("refresh_token") val refreshToken : String?,
+    @SerializedName("tokenType") val tokenType : String?,
+    @SerializedName("accessToken") val accessToken : String?,
+    @SerializedName("refreshToken") val refreshToken : String?,
     @SerializedName("message") val message : String?,
     @SerializedName("status") val status : String?,
 
