0523b6ad502216e7a0cb05e29c3c09a1b907bd52
박성완 <swany0509@naver.com>
2023-05-11 09:23:39 +0900

    feat : 소리 정보 수정

--- android/app/src/main/java/com/slembers/alarmony/util/SoundInfo.kt
+++ android/app/src/main/java/com/slembers/alarmony/util/SoundInfo.kt
@@ -1,4 +1,4 @@
-package com.slembers.alarmony.util;
+package com.slembers.alarmony.util
 
 import android.media.MediaPlayer
 import androidx.compose.runtime.Composable
@@ -12,51 +12,71 @@ fun groupSoundInfos() : List<SoundItem> = listOf(
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Normal",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.normal)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.normal),
+        mp3Id = R.raw.normal,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Crescendo",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.crescendo)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.crescendo),
+        mp3Id = R.raw.crescendo,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "SineLoop",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.sineloop)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.sineloop),
+        mp3Id = R.raw.sineloop,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Piano",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.piano)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.piano),
+        mp3Id = R.raw.piano,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Xylophone",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.xylophone)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.xylophone),
+        mp3Id = R.raw.xylophone,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Guitar",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.guitar)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.guitar),
+        mp3Id = R.raw.guitar,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Ukulele",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.ukulele)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.ukulele),
+        mp3Id = R.raw.ukulele,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "RingRing",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.ringring)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.ringring),
+        mp3Id = R.raw.ringring,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Chicken",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.chicken)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.chicken),
+        mp3Id = R.raw.chicken,
+        imageId = R.drawable.play_button
     ),
     SoundItem(
         image = painterResource(id = R.drawable.play_button),
         name = "Horror",
-        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.horror)
+        mp3Content = MediaPlayer.create(LocalContext.current, R.raw.horror),
+        mp3Id = R.raw.horror,
+        imageId = R.drawable.play_button
     ),
 )
