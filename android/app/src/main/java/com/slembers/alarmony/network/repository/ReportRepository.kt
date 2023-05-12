51ddeb36717a8cbbef2d5d643ffcfb688528a46a
박성완 <swany0509@naver.com>
2023-05-12 16:06:40 +0900

    feat : 신고 관련 API 연동

--- android/app/src/main/java/com/slembers/alarmony/network/repository/ReportRepository.kt
+++ android/app/src/main/java/com/slembers/alarmony/network/repository/ReportRepository.kt
@@ -1,9 +1,14 @@
 package com.slembers.alarmony.network.repository
 
 import com.slembers.alarmony.model.db.ReportRequestDto
+import com.slembers.alarmony.model.db.dto.ReportDto
+import com.slembers.alarmony.model.db.dto.ReportListDto
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ReportRepository {
 
@@ -11,4 +16,13 @@ interface ReportRepository {
     fun createReport(
         @Body reportRequestDto: ReportRequestDto
     ) : Call<Unit>
+
+    @GET("reports")
+    suspend fun getReportList(
+    ) : Response<ReportListDto>
+
+    @GET("reports/{reportId}")
+    suspend fun getReportDetail(
+        @Path("reportId", encoded = true) reportId : Long?,
+    ): Response<ReportDto>
 }
--- android/app/src/main/java/com/slembers/alarmony/network/service/ReportService.kt
+++ android/app/src/main/java/com/slembers/alarmony/network/service/ReportService.kt
@@ -2,8 +2,11 @@ package com.slembers.alarmony.network.service
 
 import android.util.Log
 import com.slembers.alarmony.model.db.ReportRequestDto
-import com.slembers.alarmony.model.db.dto.MemberListDto
+import com.slembers.alarmony.model.db.dto.ReportDto
+import com.slembers.alarmony.model.db.dto.ReportListDto
 import com.slembers.alarmony.network.api.AlarmonyServer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,17 +15,17 @@ object ReportService {
     val reportApi = AlarmonyServer().reportApi
 
     fun createReport(
-        reportType : String?,
-        reportedNickname : String?,
-        content : String?
-    ){
+        reportType: String?,
+        reportedNickname: String?,
+        content: String?
+    ) {
         reportApi.createReport(
             ReportRequestDto(
                 reportType = reportType,
                 reportedNickname = reportedNickname,
                 content = content
             )
-        ).enqueue(object: Callback<Unit> {
+        ).enqueue(object : Callback<Unit> {
             override fun onResponse(
                 call: Call<Unit>,
                 response: Response<Unit>
@@ -34,5 +37,29 @@ object ReportService {
                 Log.i("response", "신고 생성 실패 $t")
             }
         })
+    }
+
+    suspend fun getReportList(): ReportListDto? {
+        return try {
+            val result = withContext(Dispatchers.IO) {
+                reportApi.getReportList().body()
+            }
+            result
+        } catch (e: Exception) {
+            Log.i("response", "신고 목록 가져오기 실패 $e")
+            null
+        }
+    }
+
+    suspend fun getReportDetail(reportId: Long): ReportDto? {
+        return try {
+            val result = withContext(Dispatchers.IO) {
+                reportApi.getReportDetail(reportId).body()
+            }
+            result
+        } catch (e: Exception) {
+            Log.i("response", "신고 상세 가져오기 실패 $e")
+            null
+        }
     }
 }
