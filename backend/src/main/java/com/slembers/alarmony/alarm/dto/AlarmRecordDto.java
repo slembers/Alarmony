6231ad366fc7c1e56837e519e4eb99aa5fc5d0d2
sbsc20 <sbsc20@naver.com>
2023-05-14 18:56:01 +0900

    fix: 일어나지 않은 사람 response에 message 추가

--- backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmRecordDto.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmRecordDto.java
@@ -1,5 +1,6 @@
 package com.slembers.alarmony.alarm.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,10 +8,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // Null 값인 필드 제외
 public class AlarmRecordDto {
 
     String nickname;
     String profileImg;
     boolean success;
+    String message;
 
 }
