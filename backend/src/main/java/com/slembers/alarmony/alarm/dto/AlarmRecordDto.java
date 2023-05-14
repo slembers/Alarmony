7fbe1a106a8123e1cbb08ccc2515acd17b79b6b0
sbsc20 <sbsc20@naver.com>
2023-05-14 19:09:47 +0900

    fix: null이어도 출력하도록 수정

--- backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmRecordDto.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/dto/AlarmRecordDto.java
@@ -1,6 +1,5 @@
 package com.slembers.alarmony.alarm.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +7,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @Builder
-@JsonInclude(JsonInclude.Include.NON_NULL) // Null 값인 필드 제외
 public class AlarmRecordDto {
 
     String nickname;
