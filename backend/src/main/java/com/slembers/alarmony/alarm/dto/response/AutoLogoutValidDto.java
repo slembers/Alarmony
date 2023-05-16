00262a8540abc7d259106ded9c745606ccced067
박성완 <swany0509@naver.com>
2023-05-16 22:34:59 +0900

    feat : 자동 로그아웃 request 및 response DTO 추가

---
+++ backend/src/main/java/com/slembers/alarmony/alarm/dto/response/AutoLogoutValidDto.java
@@ -0,0 +1,11 @@
+package com.slembers.alarmony.alarm.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AutoLogoutValidDto {
+
+    private boolean success;
+}
