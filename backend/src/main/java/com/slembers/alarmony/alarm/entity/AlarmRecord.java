1e7e0619cdbdc2b8a152eb232f8fa02e8935b4fa
박성완 <swany0509@naver.com>
2023-05-17 18:59:39 +0900

    feat : 알람 서버 시간 바꾸기 로그 찍기

--- backend/src/main/java/com/slembers/alarmony/alarm/entity/AlarmRecord.java
+++ backend/src/main/java/com/slembers/alarmony/alarm/entity/AlarmRecord.java
@@ -2,6 +2,7 @@ package com.slembers.alarmony.alarm.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
@@ -14,6 +15,7 @@ import java.time.LocalTime;
 
 @Entity(name = "alarm_record")
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @DynamicInsert
