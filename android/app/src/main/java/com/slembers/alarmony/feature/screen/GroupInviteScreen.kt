21f9b53fbdd79d75bfd004b978df513e300c7a58
홍금비 <canbe159@gmail.com>
2023-05-17 11:08:42 +0900

    design: Column에 padding값 추가

--- android/app/src/main/java/com/slembers/alarmony/feature/screen/GroupInviteScreen.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/screen/GroupInviteScreen.kt
@@ -2,19 +2,23 @@ package com.slembers.alarmony.feature.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.slembers.alarmony.feature.common.NavItem
+import com.slembers.alarmony.feature.common.ui.theme.toColor
 import com.slembers.alarmony.feature.ui.group.GroupBottomButtom
 import com.slembers.alarmony.feature.ui.group.GroupDialog
 import com.slembers.alarmony.feature.ui.group.GroupToolBar
@@ -41,9 +45,12 @@ fun InviteScreen(
                 navClick = { navController.popBackStack() }
             )
         },
+        containerColor = "#F9F9F9".toColor(),
         content = { innerPadding ->
             Column(
-                modifier = Modifier.padding(innerPadding),
+                modifier = Modifier.padding(innerPadding).padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+
                 content = {
                     CurrentInvite(currentMembers = currentMembers.value ?: mutableListOf())
                     SearchInviteMember(
