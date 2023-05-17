040f79bb195026f140a0e7952539f23f91ea39da
박준영 <bjy9507@naver.com>
2023-05-17 15:04:37 +0900

    fix : 요일에 상관없이 알림보내기 버튼 활성화 문제

--- android/app/src/main/java/com/slembers/alarmony/feature/ui/groupDetails/GroupDetailsMember.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/ui/groupDetails/GroupDetailsMember.kt
@@ -12,6 +12,10 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,9 +42,11 @@ fun MemberDetails(
     nickname : String = "Alarmony",
     message : String = "체크하지 않았어요...",
     isCheck : Boolean = false,
-    onClick : () -> Unit = {},
-    host : Boolean = false
+    host : Boolean = false,
+    alarmId : Long = 0
 ) {
+
+    var onAlarm by remember { mutableStateOf( true ) }
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -98,12 +104,20 @@ fun MemberDetails(
                 maxLines = 1
             )*/
         }
-        if(host && !isCheck) {
+        if(host && onAlarm && !isCheck) {
             GroupDetailsBoardBtn(
                 isCheck = false,
-                onClick = onClick
+                onClick = {
+                    onAlarm = false
+                    CoroutineScope(Dispatchers.IO).async {
+                        GroupService.notification(
+                            alarmId,
+                            nickname
+                        )
+                    }
+                }
             )
-        } else if(isCheck) {
+        } else if(!host && isCheck){
             GroupDetailsBoardBtn(
                 isCheck = true,
             )
