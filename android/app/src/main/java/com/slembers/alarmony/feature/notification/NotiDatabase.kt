3534f6bc7e6d2ae3bda427825f16cd5d173a84e7
tayo5712 <kimty5712@gmail.com>
2023-05-21 14:45:53 +0900

    Fix : 알림 id 불일치 수정

--- android/app/src/main/java/com/slembers/alarmony/feature/notification/NotiDatabase.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/notification/NotiDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Noti::class], version = 1, exportSchema = false)
+@Database(entities = [Noti::class], version = 3, exportSchema = false)
 abstract class NotiDatabase : RoomDatabase() {
     abstract fun notiDao(): NotiDao
 
--- android/app/src/main/java/com/slembers/alarmony/feature/notification/NotiManager.kt
+++ android/app/src/main/java/com/slembers/alarmony/feature/notification/NotiManager.kt
@@ -1,6 +1,7 @@
 package com.slembers.alarmony.feature.notification
 
 import android.content.Context
+import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,6 +12,8 @@ fun saveNoti(notiDto: NotiDto, context: Context) {
         val notiDao = NotiDatabase.getInstance(context).notiDao()
         repository = NotiRepository(notiDao)
         val noti : Noti = Noti.toEntity(notiDto)
+        Log.d("myResponse", "${noti.content.toString()} : 알림이 저장 되었습니다. ")
+        Log.d("myResponse", "${noti.notiId.toString()}")
         repository.addNoti(noti)
     }
 }
